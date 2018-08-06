76b15e33d33f2bb7123ce27d5f44d7c4cf47543f
Fatan <nfatah41@gmail.com>
2018-08-06 09:53:37 +0300

    add note model and update get callbacks

--- api/controllers/notes.js
+++ api/controllers/notes.js
@@ -1,29 +1,8 @@
 'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
 
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
+const db = require('../models').db
+const NoteModel = require('../models').Note
 
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
-var util = require('util');
-
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
- */
 module.exports = {
   getNotes: getNotes,
   saveNote: saveNote,
@@ -45,9 +24,14 @@ function hello(req, res) {
 }
 
 function saveNote(req, res) {
-  res.json(req)
+  NoteModel.sync({force:true}).then(() => {
+    return NoteModel.create({text: 'New Text'})
+  })
+  res.json('saved new text note')
 }
 
 function getNotes(req, res) {
-  res.json(req.path)
+  NoteModel.findAll().then(notes => {
+      res.json(notes)
+    })
 }
---
+++ api/models/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize')
+const config = require('../config')
+
+const sequelize = new Sequelize('notes', null, null, config);
+
+const Note = sequelize.define(
+  'note', {
+    text: Sequelize.STRING,
+})
+
+module.exports = {
+  db: sequelize,
+  Note: Note
+};
